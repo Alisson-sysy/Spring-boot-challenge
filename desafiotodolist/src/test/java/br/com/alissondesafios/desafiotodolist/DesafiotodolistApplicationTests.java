38754ad8a53c20a06ffb62e260ae3442da80a736
Alisson <alisson.rosa@digifred.com.br>
2024-04-05 21:49:02 -0300

    test: add unit test example - Master

---
+++ desafiotodolist/src/test/java/br/com/alissondesafios/desafiotodolist/DesafiotodolistApplicationTests.java
@@ -0,0 +1,47 @@
+package br.com.alissondesafios.desafiotodolist;
+
+import br.com.alissondesafios.desafiotodolist.entity.Todo;
+import br.com.alissondesafios.desafiotodolist.service.TodoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class DesafiotodolistApplicationTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testCreateTodoSuccess() {
+		Todo todoObject = new Todo();
+		todoObject.setiTodo(1L);
+		todoObject.setName("Breakfast");
+		todoObject.setDescricao("Make the breakfast");
+		todoObject.setFinished(Boolean.FALSE);
+		todoObject.setPrioridade(1);
+
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todoObject)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("$[0].name").isEqualTo(todoObject.getName())
+				.jsonPath("$[0].descricao").isEqualTo(todoObject.getDescricao())
+				.jsonPath("$[0].finished").isEqualTo(todoObject.getFinished())
+				.jsonPath("$[0].prioridade").isEqualTo(todoObject.getPrioridade());
+
+	}
+
+}
